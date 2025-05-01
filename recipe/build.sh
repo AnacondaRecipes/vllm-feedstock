275eaeca1c6a93f8b81f832085021959f92d1006
anaobi <anaobii@gmail.com>
2025-05-01 18:20:59 +0100

    building v0.8.1 with patch

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 set -ex
 
-# Apply patch using standard git-style -p1
 patch -p1 < "${RECIPE_DIR}/fix-gettid.patch"
 
-# Install package
 $PYTHON -m pip install . -vv --no-deps --no-build-isolation
 

363f7f355a5f88e5d247fae7a098ad02a70d793b
Andrew Howe <ahowe@anaconda.com>
2026-04-02 10:54:29 -0400

    cleanup

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,8 +6,14 @@ set -ex
 grep -q 'sys/syscall.h' csrc/cpu/utils.cpp || sed -i '1a #include <sys/syscall.h>' csrc/cpu/utils.cpp
 grep -q 'unistd.h' csrc/cpu/utils.cpp || sed -i '1a #include <unistd.h>' csrc/cpu/utils.cpp
 
-# Replace all calls to gettid() with syscall(SYS_gettid)
-sed -i 's/gettid()/syscall(SYS_gettid)/g' csrc/cpu/utils.cpp
+if [[ "$build_platform" == osx-* ]]; then
+    # Replace all calls to gettid() with syscall(SYS_gettid)
+    sed -i '.orig' 's/gettid()/syscall(SYS_gettid)/g' csrc/cpu/utils.cpp
+else
+    # Replace all calls to gettid() with syscall(SYS_gettid)
+    sed -i 's/gettid()/syscall(SYS_gettid)/g' csrc/cpu/utils.cpp
+fi
+
 
 # Proceed with installation
 $PYTHON -m pip install . -vv --no-deps --no-build-isolation

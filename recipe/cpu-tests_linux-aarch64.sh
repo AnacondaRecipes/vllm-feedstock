1c17092c3136428f53aac6503e0e4c8478e8c474
Andrew Howe <ahowe@anaconda.com>
2026-07-21 18:09:51 -0400

    A couple more PR fixes

--- recipe/cpu-tests_linux-aarch64.sh
+++ recipe/cpu-tests_linux-aarch64.sh
@@ -11,15 +11,6 @@ SKIP_TESTS=(
     --deselect tests/kernels/attention/test_cpu_attn.py::test_varlen_with_paged_kv_sink
 )
 
-# SKIP_MODELS=(
-#     google/gemma-2-2b-it
-#     or google/gemma-1.1-2b-it
-#     or google/gemma-1.1-2b-it
-#     or llama/Llama-3.2-1B-Instruct
-# )
-# expanded_skip_models=${skip_models[@]}
-
-
 pytest -v -s tests/kernels/attention/test_cpu_attn.py ${SKIP_TESTS[@]}
 
 # Skip the tests that require BFloat16

efa4dcf5e50849957acada05b62d7a824b75e3ee
Andrew Howe <ahowe@anaconda.com>
2026-07-21 14:11:02 -0400

    PR requested fixes
     * Limit to one CUDA version for CPU builds to limit extraneous builds
     * buildnum -> 0
     * fix aiohttp version pin
     * fix setuptools version pins
     * remove scipy and datasets -- they are optional dependencies
     * fix jinja pin
     * remove selector for "packaging", it is required for all builds
     * cleanup test file

--- recipe/cpu-tests_linux-64.sh
+++ recipe/cpu-tests_linux-64.sh
@@ -11,15 +11,6 @@ SKIP_TESTS=(
     --deselect tests/kernels/attention/test_cpu_attn.py::test_varlen_with_paged_kv_sink
 )
 
-# SKIP_MODELS=(
-#     google/gemma-2-2b-it
-#     or google/gemma-1.1-2b-it
-#     or google/gemma-1.1-2b-it
-#     or llama/Llama-3.2-1B-Instruct
-# )
-# expanded_skip_models=${skip_models[@]}
-
-
 pytest -v -s tests/kernels/attention/test_cpu_attn.py ${SKIP_TESTS[@]}
 
 # too many failing tests due to bfloat precision issues
@@ -38,8 +29,10 @@ pytest -v -s tests/kernels/test_onednn.py
 #pytest -v -s tests/kernels/test_awq_int4_to_int8.py
 #pytest -v -s tests/kernels/quantization/test_cpu_fp8_scaled_mm.py
 
-# Skipping this first test because it takes too long to run
-# VLLM_CPU_KVCACHE_SPACE=4 pytest -v -s tests/models/language/generation -m cpu_model -k "not (expanded_skip_models)"
+# Skipping this first test because it takes too long to run, even though it is in the upstream
+# project's list of CPU tests.
+# VLLM_CPU_KVCACHE_SPACE=4 pytest -v -s tests/models/language/generation -m cpu_model
+
 VLLM_CPU_KVCACHE_SPACE=4 pytest -v -s tests/models/language/pooling -m cpu_model --deselect "tests/models/language/pooling/test_embedding.py::test_models[ssmits/Qwen2-7B-Instruct-embed-base]"
 
 VLLM_CPU_KVCACHE_SPACE=4 pytest -v -s tests/quantization/test_compressed_tensors.py::test_compressed_tensors_w8a8_logprobs
